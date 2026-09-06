f9a0aa83bf49dcdce1c4d53583c60c8f00b3bbf4
pengging2 <kuho6063@gmail.com>
2026-09-06 15:02:01 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#41624f",
+      secondary: "#e8e4dd",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
